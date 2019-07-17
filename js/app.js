ef7d0eac4c93d653a2908bf701c586b6be0e3f2c
Bes <sejrani.besjan@gmail.com>
2019-07-18 01:35:52 +0200

    install button dissapears after accepting installation

--- js/app.js
+++ js/app.js
@@ -11,34 +11,32 @@ const button = document.getElementById("fab");
 button.classList.add("hide");
 let defferedPrompt;
 
-window.addEventListener("load", () => {
-  window.addEventListener("beforeinstallprompt", (event) => {
-    event.preventDefault();
-    defferedPrompt = event;
-    button.classList.remove("hide");
-    return false;
-  });
-
-  button.addEventListener("click", () => {
-    button.classList.add("hide");
-    if (defferedPrompt) {
-      defferedPrompt.prompt();
-
-      defferedPrompt.userChoice.then((choiceResult) => {
-        console.log(choiceResult.outcome);
-
-        if (choiceResult.outcome === "dismissed") {
-          console.log("user cancelled installation");
-        } else {
-          console.log("user added to homescreen");
-        }
-        deferredPrompt = null;
-      });
-    }
-  });
-
-  window.onappinstalled = (event) => {
-    console.log("app uninstalled");
-    button.classList.add("hide");
-  };
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  defferedPrompt = event;
+  button.classList.remove("hide");
+  return false;
 });
+
+button.addEventListener("click", () => {
+  button.classList.add("hide");
+  if (defferedPrompt) {
+    defferedPrompt.prompt();
+
+    defferedPrompt.userChoice.then((choiceResult) => {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === "dismissed") {
+        console.log("user cancelled installation");
+      } else {
+        console.log("user added to homescreen");
+      }
+      deferredPrompt = null;
+    });
+  }
+});
+
+/* window.onappinstalled = (event) => {
+  console.log("app uninstalled");
+  button.classList.add("hide");
+}; */
