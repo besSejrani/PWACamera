5f5c45b8975b3098354e8b7f0638fe1d891482fc
Bes <sejrani.besjan@gmail.com>
2019-07-22 09:47:35 +0200

    added pure css slider

--- js/materialize.js
+++ js/materialize.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   /*   M.AutoInit(); */
-  const elems = document.querySelectorAll(".carousel");
+  /* const elems = document.querySelectorAll(".carousel");
   M.Carousel.init(elems, {
     fullWidth: true,
     duration: 150,
     shift: 0,
     indicators: true,
     noWrap: false
-  });
-
-  setInterval(() => {
-    $(".carousel").carousel("next");
-  }, 3500);
+  }); */
 });
