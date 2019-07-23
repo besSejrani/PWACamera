370ad382e4a1abad333120392e4eddcaccadb1cb
Bes <sejrani.besjan@gmail.com>
2019-07-23 04:33:05 +0200

    added products

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@
 | Setting assets, static cache & dynamic cache
 |--------------------------------------------------
 */
-const static = "static_v2";
-const dynamic = "dynamic_v2";
+const static = "static_v4";
+const dynamic = "dynamic_v4";
 const assets = [
   "/index.html",
   "/404.html",
