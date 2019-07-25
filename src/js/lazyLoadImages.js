c37823ab804d8a0d52ac23dc3fa0bf4c4b405e9d
Bes <sejrani.besjan@gmail.com>
2019-07-25 06:25:00 +0200

    added intersecting observer

--- src/js/lazyLoadImages.js
+++ src/js/lazyLoadImages.js
@@ -22,3 +22,24 @@ const lazyLoad = (target) => {
 };
 
 targets.forEach(lazyLoad);
+
+/* document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages;
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    var imageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function(image) {
+      imageObserver.observe(image);
+    });
+  }
+}); */
