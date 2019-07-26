9424b14e639b3e913199e6b765b90b285a8c4bc2
Bes <sejrani.besjan@gmail.com>
2019-07-26 16:18:37 +0200

    added lazy loading with blur effect first image

--- src/js/lazyLoadImages.js
+++ src/js/lazyLoadImages.js
@@ -9,9 +9,8 @@ const lazyLoad = (target) => {
       if (entry.isIntersecting) {
         const img = entry.target;
         const src = img.getAttribute("data-lazy");
-
         img.setAttribute("src", src);
-        /* img.classList.add("fade"); */
+        img.classList.remove("progessiveImage");
 
         observer.disconnect();
       }
