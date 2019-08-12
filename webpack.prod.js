62302901917b8cbfa46eb1c75bce5cb7ee4c05e0
Bes <sejrani.besjan@gmail.com>
2019-08-12 22:01:28 +0200

    added webp fallback

--- webpack.prod.js
+++ webpack.prod.js
@@ -56,7 +56,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ["@babel/env"],
             plugins: []
           }
         }
