3f285b22383753e0046921d7247868a12a96d8fb
Bes <besjan.sejrani@outlook.fr>
2020-11-20 15:49:45 +0100

    added another webpack manifest.json pwa plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -149,7 +149,7 @@ module.exports = {
         use: {
           loader: "file-loader",
           options: {
-            name: "[name].[hash][ext]",
+            name: "[name].[fullhash][ext]",
             outputPath: "img",
           },
         },
